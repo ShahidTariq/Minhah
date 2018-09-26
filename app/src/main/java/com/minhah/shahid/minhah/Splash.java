d1637de1152c8c154146ceb784fed013f773f2e3
ShahidTariqLone <tariq@trialx.com>
2018-09-26 19:36:42 +0530

    Added navigation drawer and changed color defaults

--- app/src/main/java/com/minhah/shahid/minhah/Splash.java
+++ app/src/main/java/com/minhah/shahid/minhah/Splash.java
@@ -1,13 +1,44 @@
 package com.minhah.shahid.minhah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
+    ImageView splashImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splashImage = (ImageView) findViewById(R.id.splash_image);
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.scale_anim);
+        splashImage.setAnimation(anim);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+
+                    Intent intent = new Intent(Splash.this,
+                            MainActivity.class);
+                    startActivity(intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
